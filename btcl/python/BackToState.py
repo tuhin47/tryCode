a98d46932ce3431d314192467207524db6788017
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-01-14 13:22:17 +0600

    modify btcl script

--- btcl/python/BackToState.py
+++ btcl/python/BackToState.py
@@ -4,11 +4,17 @@ arr = open("python/temp.log","r").readlines()
 back.write("#--"+"".join(arr))
 
 back.write("SET FOREIGN_KEY_CHECKS=0;\n\n")
-
+flag=True
 for x in open("python/generatedDiff.log","r").readlines():
+    if flag==True and len(x)>7 and x[0:7] == "+INSERT":
+        y=x.split(" ")
+        back.write("TRUNCATE TABLE "+y[2]+";\n")
+    flag=True
     if len(x)>7 and x[0:7] == "-INSERT":
         y=x.split(" ")
         back.write("TRUNCATE TABLE "+y[2]+";\n")
         back.write(x[1:]+"\n")
+        flag=False
+
 back.write("SET FOREIGN_KEY_CHECKS=1;\n")
 back.close()
